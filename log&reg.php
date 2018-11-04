eb4a7b29036247819ae0adbd11c76d2eac0eb428
mr.cat <ward.pille@student.ehb.be>
2018-11-04 17:28:17 +0100

    yes

--- log&reg.php
+++ log&reg.php
@@ -1,34 +1,47 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title>log&amp;reg</title>
-        <script type = "text/javascript">
-            function showFormLog() {document.getElementById("logForm").style.display = "block";}
-            function showFormReg() {document.getElementById("regForm").style.display = "block";}
-            function deleteFormLog() {document.getElementById("logForm").style.display = "none";}
-            function deleteFormReg() {document.getElementById("regForm").style.display = "none";}
-        </script>
-        <link rel="stylesheet" type="text/css" href="css/logPageStyle.css"/>.
-    </head>
-    <body>
-        <div class="main-container">
-            <img src="pics/logo_bx.png" width="400px" height="410px" alt="BX-logo"/>
-            <button name="Log" onclick="deleteFormReg(),showFormLog()">Login</button>
-            <button name="Reg" onclick="deleteFormLog(),showFormReg()">Register</button>
-            <form name="logForm" id="logForm" action="php/login.db.php" method="post" style="display:none">
-                <input name="email" type="email" placeholder="E-mail"/>
-                <input name="pwd" type="password" placeholder="Password"/>
-                <button name="sublogin" type="submit">Login</button>
-            </form>
-            <form name="regForm" id="regForm" action="php/register.db.php" method="post" style="display:none">
-                <input name="email" type="email" placeholder="E-mail"/>
-                <input name="pwd" type="password" placeholder="Password"/>
-                <input name="ckpwd" type="password" placeholder="Confirm Password"/>
-                <button name="subRegister" type="submit">Register</button>
-            </form>
-        </div>
-        <footer>
+
+<head>
+    <title>log&amp;reg</title>
+    <script type="text/javascript">
+        function showFormLog() {
+            document.getElementById("logForm").style.display = "block";
+        }
+
+        function showFormReg() {
+            document.getElementById("regForm").style.display = "block";
+        }
+
+        function deleteFormLog() {
+            document.getElementById("logForm").style.display = "none";
+        }
+
+        function deleteFormReg() {
+            document.getElementById("regForm").style.display = "none";
+        }
+    </script>
+    <link rel="stylesheet" type="text/css" href="css/logPageStyle.css" />.
+</head>
+
+<body>
+    <div class="main-container">
+        <img src="pics/logo_bx.png" width="400px" height="410px" alt="BX-logo" />
+        <button name="Log" onclick="deleteFormReg(),showFormLog()">Login</button>
+        <button name="Reg" onclick="deleteFormLog(),showFormReg()">Register</button>
+        <form name="logForm" id="logForm" action="php/login.db.php" method="post" style="display:none">
+            <input name="email" type="email" placeholder="E-mail" />
+            <input name="pwd" type="password" placeholder="Password" />
+            <button name="sublogin" type="submit">Login</button>
+        </form>
+        <form name="regForm" id="regForm" action="php/register.db.php" method="post" style="display:none">
+            <input name="email" type="email" placeholder="E-mail" />
+            <input name="pwd" type="password" placeholder="Password" />
+            <input name="ckpwd" type="password" placeholder="Confirm Password" />
+            <button name="subRegister" type="submit">Register</button>
+        </form>
+    </div>
+    <footer>
         <div class="footer-box">
             <lu>
                 <li><a href="http://www.bxbrussels.com/nl/copyright/">© Copyright BX Brussels</a></li>
@@ -37,5 +50,6 @@
             </lu>
         </div>
     </footer>
-    </body>
+</body>
+
 </html>
